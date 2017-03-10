dd4d287cf3ac3041556d63665e6b50155e9b58af
Elizabeth Nicholson <elizabethnich19@gmail.com>
2017-03-10 19:10:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection)
+  new_collection = []
+  if block_given?
+    i = 0
+    while i < collection.length
+      new_collection.push(yield collection[i])
+      i += 1
+    end
+    new_collection
+  end
+end
